2b2755c8eb2c1e31abb5353b57d8c70284988c5f
zhuhui <huizhu@wisedu.com>
2017-08-25 17:22:15 +0800

    完善select组件

--- packages/select/src/select.vue
+++ packages/select/src/select.vue
@@ -4,7 +4,7 @@
       <!-- select 模板 -->
       <template v-if="type === 'select'">{{singleSelectDisplay()}}</template>
       <!-- multi-select 模板 -->
-      <template v-if="type === 'multi-select'">{{scope.value.map(item => item.name).join(',')}}</template>
+      <template v-if="type === 'multi-select'">{{multiSelectDisplay()}}</template>
       <!-- 自定义 -->
       <slot v-else  name="display" :options="options" :value="value"></slot>
     </div>
@@ -14,6 +14,11 @@
         <template v-if="type === 'select'">
           <mt-radio v-model="currentValue" :options="getOptions_select(options)" @change="handleClick_select"></mt-radio>
         </template>
+        <!-- multi-select 模板 -->
+        <template v-if="type === 'multi-select'">
+          <mt-checklist v-model="currentValue" :options="getOptions_select(options)" @change="handleClick_multiSelect"></mt-checklist>
+          <selected-footer :options="options" v-model="currentValue" @confirm-click="selectorShow = false"></selected-footer>
+        </template>
         <!-- 自定义 -->
         <slot v-else name="selector" :options="options" :value="value"></slot>
       </div>
@@ -21,12 +26,14 @@
   </mt-cell>
 </template>
 <script>
+import SelectedFooter from './selected-footer.vue';
 /**
  * @event selector-click - 点击选择器触发的事件
  */
 /* eslint-disable */
 export default {
   name: 'mt-select',
+  components: { SelectedFooter },
   props: {
     value: { required: true },
     url: { type: String, default: '' },
@@ -41,14 +48,25 @@ export default {
     };
   },
   computed: {
+    /***** select 专有属性 *****/
     currentValue: {
       get () {
-        return this.value;
+        if (this.type === 'select') {
+          return this.value;
+        } else if (this.type === 'multi-select') {
+          if (this.value === '') return []
+          return this.value.split(',')
+        }
       }, 
       set (val) {
-        this.$emit('input', val.toString());
+        if (this.type === 'select') {
+          this.$emit('input', val.toString());
+        } else if (this.type === 'multi-select') {
+          this.$emit('input', val.join(','))
+        }
       }
     }
+    /***** select 专有属性 end *****/
   },
   methods: {
     handleDisplayClick(e) {
@@ -83,8 +101,22 @@ export default {
       this.$emit('change', val)
       this.selectorShow = false
       history.back()
-    }
+    },
     /***** select 专有方法 end *****/
+
+    /***** multi-select 专有方法 *****/
+    multiSelectDisplay () {
+      if (this.value === '') return this.placeholder
+      if (this.options.length === 0) {
+        this.$emit('selector-click', '')
+        return ''
+      }
+      return this.options.filter(item => this.currentValue.indexOf(item.id.toString()) > -1).map(item => item.name).join(',')
+    },
+    handleClick_multiSelect (val) {
+      this.$emit('change', this.currentValue)
+    }
+    /***** multi-select 专有方法 end *****/
   },
   mounted () {
     window.addEventListener("popstate", this.historyChange)
@@ -95,20 +127,19 @@ export default {
 };
 </script>
 <style>
+@import "../../../src/style/var.css";
 .slide-enter-active, .slide-leave-active {
   transition: transform .25s
 }
 .slide-enter, .slide-leave-to /* .slide-leave-active in below version 2.1.8 */ {
   transform: translateX(100%);
 }
-.select-value {
-}
 .select-container {
   position: fixed;
   width: 100%;
   height: 100%;
   /* background: rgba(0, 0, 0, 0.4); */
-  background: #fff;
+  background: $bg-lv2;
   left: 0;
   top: 0;
   z-index: 999;
---
+++ packages/select/src/selected-footer.vue
@@ -0,0 +1,140 @@
+<template>
+  <div class="mint-selected-footer">
+    <div v-show="!isShowDetail" class="mint-selected-footer-bar">
+      <mt-button class="mint-selected-footer-detail" :disabled="value.length === 0" size="large" @click="isShowDetail = true">查看已选({{value.length}})</mt-button>
+      <mt-button class="mint-selected-footer-confirm" size="large" type="primary" @click="handleConfirm">确定</mt-button>
+    </div>
+    <div v-show="isShowDetail" class="mint-selected-footer-list-mask"></div>
+    <div v-show="isShowDetail" class="mint-selected-footer-list-container">
+      <div class="mint-selected-footer-list-header">
+        <a href="javascript:void(0)" @click="handleCancel">取消</a>
+        <span>已选({{value.length}})</span>
+        <a @click="handleClickListConfirm" class="mint-selected-footer-list-confirm" href="javascript:void(0)">确定</a>
+      </div>
+      <div class="mint-selected-footer-list">
+        <mt-checklist v-model="currentValue" :options="listData" ></mt-checklist>
+      </div>
+    </div>
+  </div>  
+</template>
+<script>
+export default {
+  props: {
+    options: {required: true, type: Array},
+    value: {required: true, type: Array}
+  },
+  data() {
+    return {
+      isShowDetail: false,
+      currentValue: []
+    };
+  },
+  watch: {
+    value(val) {
+      this.currentValue = val.slice(0, this.value.length);
+    }
+  },
+  computed: {
+    listData() {
+      if (this.value.length === 0 || this.options.length === 0) {
+        return [];
+      }
+      return this.options.filter(item => {
+        return this.value.indexOf(item.id.toString()) > -1;
+      }).map(item => {
+        return {
+          label: item.name,
+          value: item.id
+        };
+      });
+    }
+  },
+  methods: {
+    handleCancel() {
+      this.currentValue = JSON.parse(JSON.stringify(this.value));
+      this.isShowDetail = false;
+      setTimeout(_ => {
+      });
+    },
+    handleClickListConfirm() {
+      this.$emit('input', this.currentValue);
+      this.isShowDetail = false;
+    },
+    handleConfirm() {
+      this.$emit('confirm-click');
+    }
+  },
+  created() {
+    this.currentValue = this.value.slice(0, this.value.length);
+  }
+};
+</script>
+<style>
+@import "../../../src/style/var.css";
+.mint-selected-footer {
+  position: absolute;
+  width: 100%;
+  border-top: 1px solid $grey-lv5;
+  background: $bg-lv2;
+  padding: 8px 12px;
+  left: 0;
+  bottom: 0;
+}
+
+.mint-selected-footer-bar {
+  display: flex;
+  width: 100%;
+}
+.mint-selected-footer .mint-button {
+  margin-top: 0;
+  width: auto;
+}
+.mint-selected-footer .mint-selected-footer-confirm {
+  width: 80px;
+  flex-shrink: 0;
+}
+.mint-selected-footer .mint-selected-footer-detail {
+  flex-grow: 1;
+  margin-right: 12px;
+}
+.mint-selected-footer-list-container {
+  position: fixed;
+  width: 100%;
+  left: 0;
+  bottom: 0;
+  background: $bg-lv2;
+  min-height: 50%;
+  max-height: 80%;
+}
+.mint-selected-footer-list-header {
+  display: flex;
+  text-align: center;
+  border-top: 1px solid $grey-lv5;
+  border-bottom: 1px solid $grey-lv5;
+  padding: 12px 0;
+}
+.mint-selected-footer-list-header a {
+  flex-grow: 0;
+  padding: 0 12px;
+}
+.mint-selected-footer-list-confirm {
+  color: $color-green;
+}
+.mint-selected-footer-list-header span {
+  flex-grow: 1;
+}
+.mint-selected-footer-list-container .mint-checklist-title {
+  display: none;
+}
+.mint-selected-footer-list-mask {
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  background: rgba(0, 0, 0, 0.4);
+}
+.mint-selected-footer-list {
+  overflow: auto;
+}
+</style>
