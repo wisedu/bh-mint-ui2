2672b3848d9b0a61811b94868d18a5dd292b2a46
guduziye <05123875lgh@163.com>
2017-09-12 09:36:14 +0800

    添加容器组件

---
+++ packages/layout-list/src/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div @click="handleClick" class="mint-layout-list">
+    <slot></slot>
+  </div>
+</template>
+<script>
+    /**
+     * @noteType component
+     * @name LayoutList
+     * @tagName mt-layout-list
+     * @desc 列表容器
+     * @type containerWrap
+     * @wrapClassName mint-layout-list
+     * @html
+     * <mt-layout-list></mt-layout-list>
+     */
+    export default {
+        name: 'mt-layout-list',
+        methods: {
+            handleClick(evt) {
+                this.$emit('click', evt);
+            }
+        }
+    };
+</script>
+<style lang="css">
+  @import "../../../src/style/var.css";
+  @component-namespace mint {
+    @component layout-list {
+        position: relative;
+
+        &:after{
+          content: "";
+          display: table;
+          clear: both;
+         }
+    }
+  }
+
+</style>
