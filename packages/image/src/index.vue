5adf05385716c071fd298a6dd3651699cf0e976a
guduziye <05123875lgh@163.com>
2017-09-15 16:55:34 +0800

    图片添加水印功能

--- packages/image/src/index.vue
+++ packages/image/src/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <img @click="handleClick" class="mint-image" :src="src" :style="computeStyle" />
+  <img @click="handleClick" class="mint-image" :watermark="watermark" :src="src" :style="computeStyle" />
 </template>
 
 <script>
@@ -43,12 +43,51 @@ export default {
        * @type input
        * @value
        */
-      height: String
+      height: String,
+      /**
+       * @noteType prop
+       * @field watermark
+       * @desc 生成水印
+       * @valueType boolean
+       * @type radio
+       * @value false
+       * @option
+       [
+       {
+           "text": "否",
+           "value": false
+       },
+       {
+           "text": "是",
+           "value": true
+       }
+       ]
+       */
+      watermark: Boolean,
+      /**
+       * @noteType prop
+       * @field top
+       * @desc top
+       * @type input
+       * @value
+       */
+      top: String,
+      /**
+       * @noteType prop
+       * @field left
+       * @desc left
+       * @type input
+       * @value
+       */
+      left: String
   },
     computed: {
         computeStyle: function() {
             let width = this.width;
             let height = this.height;
+            let top = this.top;
+            let left = this.left;
+            let watermark = this.watermark;
             let _style = '';
             if (width) {
                 _style += 'width:' + width + ';';
@@ -56,6 +95,16 @@ export default {
             if (height) {
                 _style += 'height:' + height + ';';
             }
+            if (watermark === true || watermark === 'true') {
+                _style += 'position: fixed;';
+            }
+            if (top) {
+                _style += 'top:' + top + ';';
+            }
+            if (left) {
+                _style += 'left:' + left + ';';
+            }
+
             return _style;
         }
     }
