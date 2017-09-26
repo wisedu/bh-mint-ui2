80a76573d96c87d8fd6c6e9a2a7bf08ac8399c75
guduziye <05123875lgh@163.com>
2017-09-26 09:26:15 +0800

    添加示例

--- example/pages/layout-left-right.vue
+++ example/pages/layout-left-right.vue
@@ -1,11 +1,19 @@
 <template>
   <div style="margin-top: 60px;">
-    <mt-layout-left-right>
-      <mt-layout-left-right-item slot="left" position="left" percentage="30"><div class="aaa">111</div></mt-layout-left-right-item>
-      <mt-layout-left-right-item slot="right" position="right" percentage="70"><div class="bbb">222</div></mt-layout-left-right-item>
-    </mt-layout-left-right>
-  </div>
+    <div style="margin-top: 20px;">
+      <mt-layout-left-right>
+        <mt-layout-left-right-item slot="left" position="left" percentage="30"><div class="aaa">111</div></mt-layout-left-right-item>
+        <mt-layout-left-right-item slot="right" position="right" percentage="70"><div class="bbb">222</div></mt-layout-left-right-item>
+      </mt-layout-left-right>
+    </div>
 
+    <div style="margin-top: 20px;">
+      <mt-layout-left-right>
+        <mt-layout-left-right-item slot="left" position="left" percentage="40px"><div>左侧40px</div></mt-layout-left-right-item>
+        <mt-layout-left-right-item slot="right" position="right" percentage="calc(100% - 40px)"><div>右侧</div></mt-layout-left-right-item>
+      </mt-layout-left-right>
+    </div>
+  </div>
 </template>
 
 <script>
