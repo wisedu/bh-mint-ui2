9dedea3116babc3eb307e9a72e862548c9cd49cf
shizhan-PC\shizhan <1533688026@qq.com>
2017-11-10 14:02:27 +0800

    增加icon组件

--- example/index.tpl
+++ example/index.tpl
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <title>Mint UI</title>
     <link rel="stylesheet" href="//at.alicdn.com/t/font_1469597443_9221172.css">
-    <link rel="stylesheet" href="http://res.wisedu.com/fe_components/iconfont_mobile/demo_fontclass.html">
+    <link rel="stylesheet" href="http://res.wisedu.com/fe_components/iconfont_mobile/iconfont.css">
+    <link rel="stylesheet" href="https://daneden.github.io/animate.css/animate.min.css">
   </head>
   <body>
     <div id="app"></div><% if (process.env.NODE_ENV === 'production') { %>
---
+++ packages/icon/src/icon.vue
@@ -0,0 +1,92 @@
+<template>
+    <span class="box" :class="animateClass">
+        <i @click="handleClick" class="iconfont" :style="computeStyle" :class="type">
+        </i>
+    </span>
+
+</template>
+<script>
+    /**
+     * @noteType component
+     * @name Icon
+     * @tagName mt-icon
+     * @desc 分隔线
+     * @wrapClassName mint-icon
+     * @html
+     * <mt-icon></mt-icon>
+     */
+    export default {
+        name: 'mt-icon',
+        methods: {
+            handleClick(evt) {
+                this.$emit('click', evt);
+                if(this.changeColor){
+                    this.isAddbcColor = this.changeColor;
+                }
+                if(this.animate){
+                    this.animateClass = this.animate;
+                }
+            }
+        },
+        props: {
+            fontSize: String,
+            animate: String,
+            changeColor: String,
+            type: String,
+            color: String,
+            /**
+             * @type 图标样式
+             * @fontSize 图标大小
+             * @changeColor 点击之后颜色
+             * @animate css动画
+             * @color 图标颜色
+             * @padding 图标间距
+             */
+            padding: String
+        },
+        data(){
+            return {
+                isAddbcColor:'',
+                animateClass:''
+            };
+        },
+        computed: {
+            computeStyle: function() {
+                let _style = '';
+                let fontSize = this.fontSize;
+                let padding = this.padding;
+                let color = this.color;
+                let isAddbcColor = this.isAddbcColor;
+                if (fontSize) {
+                    _style += `font-size: ${fontSize};`;
+                }
+                if (padding) {
+                    _style += `padding: ${padding};`;
+                }
+                if (color) {
+                    _style += `color: ${color};`;
+                }
+                if (isAddbcColor) {
+                    _style += `color: ${isAddbcColor};`;
+                }
+                return _style;
+            }
+        }
+    };
+</script>
+<style lang="css">
+  @import "../../../src/style/var.css";
+  @component-namespace mint {
+    @component icon {
+      position: relative;
+      height: 1px;
+      background: #ddd;
+    }
+  }
+    .box{
+        display: inline-block;
+    }
+    i{
+        display:inline-block;
+    }
+</style>
