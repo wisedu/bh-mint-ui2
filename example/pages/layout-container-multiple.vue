5e39e65475d28b93ce569699a1fd846d4307d527
guduziye <05123875lgh@163.com>
2017-11-10 14:02:07 +0800

    多列容器

---
+++ example/pages/layout-container-multiple.vue
@@ -0,0 +1,32 @@
+<template>
+    <div style="margin-top: 60px;">
+        <div>示例一</div>
+        <div style="margin-top: 20px;">
+            <mint-layout-container-multiple>
+                <mint-layout-container-multiple-item style="width: 33.33%">左</mint-layout-container-multiple-item>
+                <mint-layout-container-multiple-item style="width: 33.33%">中</mint-layout-container-multiple-item>
+                <mint-layout-container-multiple-item style="width: 33.33%">右</mint-layout-container-multiple-item>
+            </mint-layout-container-multiple>
+        </div>
+
+
+        <div style="margin-top: 20px;">
+            <div>示例二</div>
+            <mint-layout-container-multiple>
+                <mint-layout-container-multiple-item style="width: 20%">1</mint-layout-container-multiple-item>
+                <mint-layout-container-multiple-item style="width: 30%">2</mint-layout-container-multiple-item>
+                <mint-layout-container-multiple-item style="width: 10%">3</mint-layout-container-multiple-item>
+                <mint-layout-container-multiple-item style="width: 40%">4</mint-layout-container-multiple-item>
+            </mint-layout-container-multiple>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'page-layout-container-multiple'
+    };
+</script>
+
+<style lang="css" scoped>
+</style>
---
+++ packages/layout-container-multiple/src/layout-container-multiple.vue
@@ -0,0 +1,36 @@
+<template>
+  <div @click="handleClick" class="mint-layout-container-multiple">
+    <slot></slot>
+  </div>
+</template>
+<script>
+    export default {
+        name: 'mint-layout-container-multiple',
+        methods: {
+            handleClick(evt) {
+                this.$emit('click', evt);
+            }
+        },
+        props: {
+        }
+    };
+
+</script>
+<style lang="css">
+  @import "../../../src/style/var.css";
+  @component-namespace mint {
+    @component layout-container-multiple {
+        position: relative;
+        width: 100%;
+        box-sizing: border-box;
+    }
+  }
+
+  .mint-layout-container-multiple:after{
+    content: "";
+    display: table;
+    clear: both;
+  }
+
+
+</style>
