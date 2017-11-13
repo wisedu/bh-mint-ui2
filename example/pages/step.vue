17704bbf84ef4ecd6625c0398f6ac9e6509b172d
戚雨 <yuqi@wisedu.com>
2017-11-13 16:41:36 +0800

    demo 调整

--- example/pages/step.vue
+++ example/pages/step.vue
@@ -2,7 +2,7 @@
     <div class="page-select">
         <mt-steps direction="horizontal">
             <mt-step status="finish">买家下单</mt-step>
-            <mt-step status="finish">商家接单</mt-step>
+            <mt-step :status="status">商家接单</mt-step>
             <mt-step status="process">买家提货</mt-step>
             <mt-step>交易完成</mt-step>
         </mt-steps>
@@ -12,7 +12,7 @@
                 <h3>【城市】物流状态1</h3>
                 <p slot="left">12:40<br>2016-07-12</p>
             </mt-step>
-            <mt-step status="process">
+            <mt-step :status="status">
                 <h3>【城市】物流状态2</h3>
                 <p slot="left">10:00<br>2016-07-11</p>
             </mt-step>
@@ -21,12 +21,32 @@
                 <p slot="left">09:30<br>2016-07-10</p>
             </mt-step>
         </mt-steps>
+        <mt-button @click="changeStatus">第二步</mt-button>
     </div>
 </template>
 
 <script>
 export default {
-
+    data:function(){
+        return {
+            status: ""
+        }
+    },
+    methods: {
+        changeStatus: function() {
+            switch (this.status) {
+                case "":
+                    this.status = "process";
+                    break;
+                case "process":
+                    this.status = "finish";
+                    break;
+                case "finish":
+                    this.status = "";
+                    break;
+            }
+        }
+    }
 }
 </script>
 
