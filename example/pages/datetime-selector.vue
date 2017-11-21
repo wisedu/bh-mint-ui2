0beec350bec6732b31f0bd07b48d0a277ba5a566
戚雨 <yuqi@wisedu.com>
2017-11-21 20:59:06 +0800

    增加示例

--- example/pages/datetime-selector.vue
+++ example/pages/datetime-selector.vue
@@ -4,6 +4,7 @@
     <div class="page-part">
       <mt-datetime-selector label="日期时间" placeholder="请输入发生时间" type="datetime" value="2017-08-02 09:01:01"></mt-datetime-selector>
       <mt-datetime-selector label="日期时间" placeholder="请输入发生时间" type="datetime" :value="null"></mt-datetime-selector>
+      <mt-datetime-selector label="日期时间" placeholder="选择年月" type="ym" :value="null"></mt-datetime-selector>
     </div>
     <div class="page-part">
       <mt-datetime-selector label="日期" placeholder="请输入生日" type="date"></mt-datetime-selector>
