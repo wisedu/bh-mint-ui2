57806ed5fc44e99e649b325c8c7efefd7fd519e6
xujiabin <xujiabin0114@163.com>
2017-11-24 14:19:54 +0800

    add subscript

--- example/pages/layout-container.vue
+++ example/pages/layout-container.vue
@@ -5,6 +5,10 @@
       @name gray
       -->
     <mt-layout-container background="gray"></mt-layout-container>
+
+    <mt-layout-container background="gray" class="layout-container">
+      <mt-subscript label="成功" fontsize="12px" background="red" width="90px" height="16px" rotate="45"></mt-subscript>
+    </mt-layout-container>
     <!--end-->
 
     <div style="margin-top: 20px;">
@@ -26,5 +30,11 @@ export default {
 <style lang="css" scoped>
   .mint-layout-container{
     height: 24px;
+  }
+  .layout-container{
+    margin-top: 16px;
+    position: relative;
+    overflow: hidden;
+    height: 70px;
   }
 </style>
