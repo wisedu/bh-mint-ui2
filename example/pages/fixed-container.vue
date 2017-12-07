7a6673fe5d70b99638e6820a7988514b6f31d0e5
guduziye <05123875lgh@163.com>
2017-12-07 22:01:58 +0800

    添加浮动组件

---
+++ example/pages/fixed-container.vue
@@ -0,0 +1,23 @@
+<template>
+  <div style="margin-top: 60px;">
+    <mt-fixed-button position="top">
+      <mt-button>按钮1</mt-button>
+      <mt-button>按钮2</mt-button>
+    </mt-fixed-button>
+
+    <mt-fixed-button>
+      <mt-button>按钮3</mt-button>
+      <mt-button>按钮4</mt-button>
+    </mt-fixed-button>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'page-layout-container'
+};
+</script>
+
+<style lang="css" scoped>
+
+</style>
---
+++ packages/fixed-container/src/fixed-container.vue
@@ -0,0 +1,48 @@
+<template>
+  <div @click="handleClick" class="mint-fixed-container" :class="['mint-fixed-container--' + position]">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'mt-fixed-button',
+      methods: {
+          handleClick(evt) {
+              this.$emit('click', evt);
+          }
+      },
+    props: {
+      position: {
+        type: String,
+        default: 'bottom'
+      }
+    }
+  };
+</script>
+
+<style>
+  @import "../../../src/style/var.css";
+
+  @component-namespace mint {
+    @component fixed-container {
+      position: fixed;
+
+      @modifier top {
+        top: 0;
+        left: 0;
+      }
+
+      @modifier bottom {
+        bottom: 0;
+        left: 0;
+      }
+    }
+  }
+
+  .mint-fixed-container:after{
+    content: "";
+    display: table;
+    clear: both;
+  }
+</style>
