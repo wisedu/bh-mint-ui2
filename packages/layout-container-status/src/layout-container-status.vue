7a9417af598d600be7d51821b34ec79c1ab906a1
guduziye <05123875lgh@163.com>
2018-01-17 17:06:52 +0800

    添加状态容器

---
+++ packages/layout-container-status/src/layout-container-status.vue
@@ -0,0 +1,30 @@
+<template>
+  <div @click="handleClick" class="mint-layout-container">
+    <slot></slot>
+  </div>
+</template>
+<script>
+    /**
+     * @noteType component
+     * @name LayoutContainer
+     * @tagName mt-layout-container
+     * @desc 普通容器
+     * @type container
+     * @wrapClassName mint-layout-container
+     * @html
+     * <mt-layout-container></mt-layout-container>
+     */
+    export default {
+        name: 'mt-layout-container-status',
+        methods: {
+            handleClick(evt) {
+                this.$emit('click', evt);
+            }
+        },
+        props: {
+        }
+    };
+</script>
+<style lang="css">
+
+</style>
